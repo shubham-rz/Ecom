cc9194ad2b9e886ffcbc4343e2ff2884caf147c8
Shubham Raj <shubhamraj@Shubhams-MacBook-Air.local>
2026-01-09 19:02:44 +0530

    Initial React project

---
+++ Elephant/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold underline'>Hathi </h1>
+    </>
+  )
+}
+
+export default App
